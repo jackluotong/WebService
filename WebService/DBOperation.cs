f05b43fbd0fc550a24fbed4a895d8e8a9e9441fb
jackluotong <59988759+jackluotong@users.noreply.github.com>
2020-06-24 15:21:34 +0800

    WebService.
    WebService used for looking for datas you want.
    Some functions will be updated and  i will continue updating.

---
+++ WebService/DBOperation.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+
+
+namespace InterFaceDemo
+{
+    public class DBOperation 
+    {
+        public SqlConnection sqlCon;//connect to database
+        private string ConServerStr = "Data Source=MSH-WORK-386;Initial Catalog=Practice;Integrated Security=True";
+        //SqlConnection con = new SqlConnection(@"Data Source=服务器名; database=数据zd库名;User id=用户回名;Password=密码");
+        //构造函数
+        public DBOperation()
+        {
+            if (sqlCon == null)
+            {
+                sqlCon = new SqlConnection();
+                sqlCon.ConnectionString = ConServerStr;
+                sqlCon.Open();
+            }
+        }
+        //关闭
+        public void Dispose()
+        {
+            if (sqlCon != null)
+            {
+                sqlCon.Close();
+                sqlCon = null;
+            }
+        }
+        //从数据库获取信息 查询
+        public List<int> selectAllCaroInfor(int Phone)
+        {
+            List<int> list = new List<int>();
+            try 
+            {
+                string sql = "select Password from dbo.PhonePassword where Phone="+Phone;
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                SqlDataReader reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        list.Add(list[i]);
+                    }
+                    //list.Add(reader.GetInt32());//list.Add(reader[0].ToString());
+                    //list.Add(reader[1]);  
+                }
+                reader.Close();
+                cmd.Dispose();
+            }
+            catch (Exception)
+            {
+                list.Add(1231);
+            }
+            return list;
+        }
+        //add
+        public bool insertCargonInfo(int Phone, int Password)
+        {
+            try
+            {
+                string sql = "insert into dbo.PhonePassword(Phone,Password) values ('" + Phone + "'," + Password + ")";
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+        //delete
+        public bool deletdCargonInfo(int Phone)
+        {
+            try
+            {
+                string sql = "delete from dbo.PhonePassword where Phone =" + Phone;
+                SqlCommand cmd = new SqlCommand(sql,sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool insertCargonInfo1(string Name, string Adress)
+        {
+            try
+            { 
+                string sql = "insert into dbo.Name(Name,Adress) values ('" + Name + "'," + Adress + ")";
+                SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        //查找
+        public List<string> SerachInfo(string Name)
+        {
+            List<string> list = new List<string>();
+             DataSet ds = new DataSet();
+            try
+            {
+                StringBuilder builder = new StringBuilder();
+               
+                builder.Append("select * from dbo.Name where Name = '" + Name + "'");
+
+                
+                // string sql = "select * from dbo.Name where Name='" + Name + "'";
+                //SqlDataAdapter ad = new SqlDataAdapter(sql, ConServerStr);
+                //ad.Fill(ds); 
+                //return ds;
+
+                //SqlCommand cmd = new SqlCommand(sql, sqlCon);
+                //SqlDataReader sqlDr = cmd.ExecuteReader();
+                //if (sqlDr.Read())
+                //{
+                //    //for (int i = 0; i < list.Count; i++)
+                //    //{
+                //    //    list.Add(list[i]);
+                //    //}
+
+                //    list.Add(sqlDr[0].ToString().Trim());
+                //    list.Add(sqlDr[1].ToString().Trim());
+                //    list.Add(sqlDr[2].ToString().Trim());
+                //    list.Add(sqlDr[3].ToString().Trim());
+                //}
+                //else
+                //{
+                //    sqlDr.Close();
+                //    cmd.Dispose();
+                //    list.Add("---");
+                //    return list;
+                //}
+            }
+            catch (Exception)
+            {
+                 
+            }
+            return list;
+            //return ds;
+             
+        }
+
+       
+    }
+}
---
+++ WebService/WebService1.asmx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace InterFaceDemo
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+#if  true
+         DBOperation dBOperation = new DBOperation();
+        [WebMethod(Description = "通过ID查找")]
+        public int[] selectAllCaroInfor(int Phone)
+        {
+            return dBOperation.selectAllCaroInfor(Phone).ToArray();
+        }
+        [WebMethod(Description = "增加一条信息")]
+        public bool insertCargonInfo(int Phone,int Password)
+        {
+            return dBOperation.insertCargonInfo(Phone, Password);
+        }
+        [WebMethod(Description = "删除一条信息")]
+        public bool deleteCargoInfo(int Phone)
+        {
+            return dBOperation.deletdCargonInfo(Phone);
+        }
+        [WebMethod(Description = "增加姓名")]
+        public bool insertCargonInfo1(string Name, string Adress)
+        { 
+        return dBOperation.insertCargonInfo1(Name, Adress);
+        }
+
+        [WebMethod(Description = "查找")]
+        [ScriptMethod(UseHttpGet =true,ResponseFormat =ResponseFormat.Json)]
+        public string[] SerachInfo(string Name)
+        {
+            Context.Response.Clear();
+            Context.Response.ContentType = "application/json";
+            
+            return dBOperation.SerachInfo(Name).ToArray() ;
+        }
+        
+#endif
+    }
+}
